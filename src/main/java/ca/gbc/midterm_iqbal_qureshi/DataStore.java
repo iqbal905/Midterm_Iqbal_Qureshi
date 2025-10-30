0958077abab56896ee86847ff5329c4208e46e11
Iqbal Qureshi <101366167@georgebrown.ca>
2025-10-30 10:30:24 -0400

    data store java update

--- src/main/java/ca/gbc/midterm_iqbal_qureshi/DataStore.java
+++ src/main/java/ca/gbc/midterm_iqbal_qureshi/DataStore.java
@@ -1,4 +1,9 @@
 package ca.gbc.midterm_iqbal_qureshi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataStore {
+    public static List<Integer> history = new ArrayList<>();
 }
+
